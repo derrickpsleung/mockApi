a7f49c31653a7abbb72a1343bfb217f74a6e8b08
derrickpsleungico <derrickleungfds@gmail.com>
2021-08-01 14:47:12 +0800

    commit

--- src/main/java/com/mobile/mock/api/controller/MockApiController.java
+++ src/main/java/com/mobile/mock/api/controller/MockApiController.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MockApiController {
 
+	private static final Map<String, String> pathJsonMap = new LinkedHashMap<String, String>() {
+		private static final long serialVersionUID = 1L;
+		{
+			put("key1", "json/login.json");
+			put("key2", "json/login.json");
+		}
+	};
+
 	@GetMapping("/{p1}/{p2}")
 	@ResponseBody
 	public String twoPath(@PathVariable String p1, @PathVariable String p2) throws IOException {
 
-		String val = readAllBytesJava7("json/login.json");
-		return val;
+		List<String> list = Arrays.asList(p1, p2);
+		String key = String.join(",", list);
+		return readAllBytesJava7(pathJsonMap.get(key));
+
+	}
+
+	@GetMapping("/{p1}/{p2}/{p3}")
+	@ResponseBody
+	public String threePath(@PathVariable String p1, @PathVariable String p2, @PathVariable String p3)
+			throws IOException {
+
+		List<String> list = Arrays.asList(p1, p2, p3);
+		String key = String.join(",", list);
+		return readAllBytesJava7(pathJsonMap.get(key));
 	}
 
 	private String readAllBytesJava7(String classPath) throws IOException {
